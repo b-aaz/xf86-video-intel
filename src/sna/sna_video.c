68fe22cbd32156ba72fd8ca22c53d77481f4c33b
b-aaz <85005689+b-aaz@users.noreply.github.com>
2025-09-14 12:57:39 +0000

    sna: Use bswap32 on DFBSD too.
    DragonFlyBSD, like FreeBSD also provides bswap32 in <sys/endian.h> so we should use it.
    
    Signed-off-by: b-aaz <b-aazbsd@proton.me>

--- src/sna/sna_video.c
+++ src/sna/sna_video.c
@@ -59,7 +59,8 @@
 #include "intel_options.h"
 
 #include <xf86xv.h>
-#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__)
+#if defined(__OpenBSD__) || defined(__FreeBSD__) || defined(__NetBSD__) || \
+defined(__DragonFly__)
 #include <sys/types.h>
 #include <sys/endian.h>
 #ifdef __OpenBSD__
